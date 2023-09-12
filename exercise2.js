b57cfd6a9e3074b3d5235c7f27d65e8b73a847b2
Conor Grace <20093293@mail.wit.ie>
2023-09-12 15:51:52 +0100

    Exercise 2

---
+++ exercise2.js
@@ -0,0 +1,9 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+    const todoTitles = json.map(todo => todo.title); // Create an array of todo titles
+    console.log(todoTitles); // Display the array of todo titles
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
